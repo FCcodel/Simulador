6f478260af044f7d2a367244e3a40ce9bb1fdc4f
FCcodel <florenciac0408@gmail.com>
2024-11-25 18:34:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,23 +21,81 @@ const xScale = d3.scaleLinear().domain([0, 4 * Math.PI]).range([0, width]);
 
 const lineE = d3.line()
                 .x((d, i) => xScale(x[i]))
-                .y(d => yScale(d));
+                .y(d => yScale(d))
+                .curve(d3.curveLinear);
 
 const lineB = d3.line()
                 .x((d, i) => xScale(x[i]))
-                .y(d => yScale(d));
+                .y(d => yScale(d))
+                .curve(d3.curveLinear);
+
+// Definir gradiente de color para las ondas
+const gradientE = svg.append("defs")
+    .append("linearGradient")
+    .attr("id", "gradientE")
+    .attr("x1", "0%")
+    .attr("y1", "0%")
+    .attr("x2", "100%")
+    .attr("y2", "0%");
+gradientE.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "blue");
+gradientE.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "lightblue");
+
+const gradientB = svg.append("defs")
+    .append("linearGradient")
+    .attr("id", "gradientB")
+    .attr("x1", "0%")
+    .attr("y1", "0%")
+    .attr("x2", "100%")
+    .attr("y2", "0%");
+gradientB.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "red");
+gradientB.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "pink");
 
 const pathE = svg.append("path")
                  .datum(yE)
                  .attr("class", "lineE")
                  .attr("d", lineE)
-                 .style("stroke", "blue");
+                 .style("stroke", "url(#gradientE)")
+                 .style("fill", "none")
+                 .style("stroke-width", 2);
 
 const pathB = svg.append("path")
                  .datum(zB)
                  .attr("class", "lineB")
                  .attr("d", lineB)
-                 .style("stroke", "red");
+                 .style("stroke", "url(#gradientB)")
+                 .style("fill", "none")
+                 .style("stroke-width", 2);
+
+// Añadir eje de propagación
+svg.append("line")
+    .attr("x1", 0)
+    .attr("y1", height / 2)
+    .attr("x2", width)
+    .attr("y2", height / 2)
+    .attr("stroke", "black")
+    .attr("stroke-dasharray", "5,5")
+    .attr("stroke-width", 1)
+    .attr("marker-end", "url(#arrow)");
+
+svg.append("defs").append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 0 10 10")
+    .attr("refX", 10)
+    .attr("refY", 5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto-start-reverse")
+  .append("path")
+    .attr("d", "M0,0 L10,5 L0,10 Z")
+    .attr("fill", "black");
 
 function updateWave() {
     yE = x.map(d => amplitudE * Math.sin(frecuenciaE * d));
@@ -91,22 +149,6 @@ function animate() {
         pathE.datum(yE).attr("d", lineE);
         pathB.datum(zB).attr("d", lineB);
         i++;
-    }, 100);
-}
-
-function stopAnimation() {
-    clearInterval(interval);
-}
-
-d3.select("#play-pause").on("click", function() {
-    animRunning = !animRunning;
-    if (animRunning) {
-        animate();
-        d3.select(this).text("Pause");
-    } else {
-        stopAnimation();
-        d3.select(this).text("Play");
-    }
-});
-
-updateWave();
+    },
+    
+    //[_{{{CITATION{{{_1{](https://github.com/Genesissdh/Estructuras-Control/tree/0c75aa66757613b7a4668d3cc243ca27a8780030/Ejercicio1.php)[_{{{CITATION{{{_2{](https://github.com/medialab/personal-air-timeline/tree/b20dab5f50c9d8c6830912c832ae93aabe38c6d5/app%2Fapp.js)[_{{{CITATION{{{_3{](https://github.com/callumwebb/cwebby/tree/fd5c2416446bbb94bd22834a5ed4d8b2f970432e/static%2Fjs%2Froc%2Fconfusion-matrix.js)[_{{{CITATION{{{_4{](https://github.com/jsIsrael/d3-epidemiology-timeline/tree/f4bd356ee06af34a6389fbabe6852e96b231420f/src%2FGraph%2FsecondIterationD3.ts)[_{{{CITATION{{{_5{](https://github.com/tinpotnick/packetpeek/tree/6039dc33889552bf2051b7ccb22ebf6d49a516c4/pcap.js)[_{{{CITATION{{{_6{](https://github.com/ntropy-esa/biochar-systems-dev/tree/ecf241fe53d1a7c91e5b3e0bb26f4e6388a8fdf1/static%2Fbw2widgets%2Fwidget_plot_types.js)
